0de9d36c285acc23ea444054d0739e724d04e03a
bymyself <cbyrne@comfy.org>
2025-04-09 14:59:32 -0700

    enable legacy manager frontend during beta phase

--- comfyui_manager/__init__.py
+++ comfyui_manager/__init__.py
@@ -1,6 +1,8 @@
 import os
 import logging
 
+ENABLE_LEGACY_COMFYUI_MANAGER_FRONT_DEFAULT = True # Enable legacy ComfyUI Manager frontend while new UI is in beta phase
+
 def prestartup():
     from . import prestartup_script  # noqa: F401
     logging.info('[PRE] ComfyUI-Manager')
@@ -12,6 +14,9 @@ def start():
     from .glob import share_3rdparty  # noqa: F401
     from .glob import cm_global       # noqa: F401
 
-    if os.environ.get('ENABLE_LEGACY_COMFYUI_MANAGER_FRONT', 'false') == 'true':
-        import nodes
-        nodes.EXTENSION_WEB_DIRS['comfyui-manager-legacy'] = os.path.join(os.path.dirname(__file__), 'js')
+    if os.environ.get('ENABLE_LEGACY_COMFYUI_MANAGER_FRONT', 'false') == 'true' or ENABLE_LEGACY_COMFYUI_MANAGER_FRONT_DEFAULT:
+        try:
+            import nodes
+            nodes.EXTENSION_WEB_DIRS['comfyui-manager-legacy'] = os.path.join(os.path.dirname(__file__), 'js')
+        except Exception as e:
+            print("Error enabling legacy ComfyUI Manager frontend:", e)
